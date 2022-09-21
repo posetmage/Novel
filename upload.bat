47f0082eb21dd847daff9f11492a71e314c31083
PosetMage <PosetMage@users.noreply.github.com>
2022-09-21 23:18:15 +0800

    all

--- upload.bat
+++ upload.bat
@@ -1,6 +1,8 @@
 git config --local user.name "PosetMage"
 git config --local user.email "PosetMage@users.noreply.github.com"
+git remote set-url origin git@POM:posetmage/posetmage.github.io.git
 
+git pull
 git add .
-git commit -m "upload"
+git commit -m "all"
 git push
