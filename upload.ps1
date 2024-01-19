d17ba3ddb5297d84da40f128b04461959d8fd203
PosetMage <PosetMage@gmail.com>
2024-01-19 19:21:26 +0800

    all

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,12 @@
+cd _shared
+./upload.bat
+cd ../
+
 git config --local user.name "PosetMage"
 git config --local user.email "PosetMage@gmail.com"
 git remote set-url origin git@POM:posetmage/Novel.git
+
+git submodule update --recursive --remote
 
 git pull
 git add .
